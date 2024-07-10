1da698ab3dadc2a178f31a2359946635440fb4ce
Naim21 <2002natorres@gmail.com>
2024-07-10 08:53:23 -0300

    Initial Commit

---
+++ js/aplicacion/app.js
@@ -0,0 +1,17 @@
+console.log("Esto es de app.js");
+let num = 10;
+num = true;
+num = 'Hola';
+
+console.log(num);
+
+const DIA_SEMANA = 7;
+
+var casa124 = "Castell" //trae problemas
+console.log(casa124)
+
+let casa123, casa125, casa126;
+casa123 = "Perez";
+casa125 = "Lopez";
+casa126 = "Torres";
+
