ea096d0e99043c6d78ca0b23c891edd1acbcdba7
Naim Torres <2002natorres@gmail.com>
2024-10-23 06:18:20 -0300

    a

--- js/aplicacion/xmlAnidado.js
+++ js/aplicacion/xmlAnidado.js
@@ -6,7 +6,25 @@ const primerReq = new XMLHttpRequest();
 primerReq.addEventListener("load", function () {
     console.log("Datos recibidos");
     const objeto = JSON.parse(this.response);
-    console.log(objeto.results[0]);
+    const filmUrl = objeto.results[0].films[1];
+    console.log(filmUrl);
+
+    const filmReq = new XMLHttpRequest();
+    filmReq.addEventListener("load", function () {
+        const film = JSON.parse(this.response);
+        const titulo = film.title;
+
+        console.log(titulo)
+        // console.log(JSON.parse(this.response));
+    });
+
+    filmReq.addEventListener("error", () => {
+        console.log("Error, imposible acceder al film");
+    });
+
+    filmReq.open("GET", filmUrl);
+
+    filmReq.send();
 });
 
 primerReq.addEventListener("error", () => {
